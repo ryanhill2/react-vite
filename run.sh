4dfd06533ae3d7d5888cf8441b1441775a71ae93
ryanhill2 <ryanhill00101@gmail.com>
2024-12-07 00:25:40 +0000

    Update home page title and location to Sligo

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run npm build
+npm run build
+
+# Copy build contents to root
+cp -r dist/* .
+
+# Add all files
+git add .
+
+# Commit changes with a commit message
+git commit -m "Update home page title and location to Sligo"
+
+# Push changes to remote repository
+git push
